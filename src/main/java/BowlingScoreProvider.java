48c7f27db932e95bc4074033e93463e5d46f7911
krish yallam <krish.yallam@jdwilliams.co.uk>
2021-03-26 16:44:51 +0000

    moved getframescore logic to framescoreprovider class

--- src/main/java/BowlingScoreProvider.java
+++ src/main/java/BowlingScoreProvider.java
@@ -3,20 +3,20 @@ import java.util.List;
 
 public class BowlingScoreProvider {
 
-    public static final String INVALID_FRAME_EXPRESSION = "invalid frame expression frame:";
-    public static final String INVALID_FRAME_SIZE = "invalid frame size";
-    public static final String INVALID_DIGITS_IN_FRAME = "invalid digits in given frame:";
-    public static final String INVALID_BONUS_EXPRESSION = "invalid bonus expression";
+
+    private static final String INVALID_FRAME_SIZE = "invalid frame size";
+    private static final String INVALID_BONUS_EXPRESSION = "invalid bonus expression";
+    private static final String BONUS_SEPARATOR = "\\|\\|";
+    private static final String FRAME_SEPARATOR = "\\|";
 
 
     public int getScore(String expression) {
-        String frames = null;
-        final Integer[] result = {0};
-        String[] splitWithDoublePipe = expression.split("\\|\\|");
-        assert expression.length() - expression.replaceAll("\\|\\|", "").length() == 2 : INVALID_BONUS_EXPRESSION;
 
-        frames = splitWithDoublePipe[0];
-        List<String> framesList = Arrays.asList(frames.split("\\|"));
+        final Integer[] result = {0};
+        String[] splitWithDoublePipe = expression.split(BONUS_SEPARATOR);
+        assert expression.length() - expression.replaceAll(BONUS_SEPARATOR, "").length() == 2 : INVALID_BONUS_EXPRESSION;
+        String frames = splitWithDoublePipe[0];
+        List<String> framesList = Arrays.asList(frames.split(FRAME_SEPARATOR));
 
         assert framesList.size() == 10 : INVALID_FRAME_SIZE;
         framesList.forEach(frame -> frameValidationAndCalculateScore(frame, result));
@@ -25,53 +25,8 @@ public class BowlingScoreProvider {
     }
 
     private void frameValidationAndCalculateScore(String frame, Integer[] result) {
-        FrameType frameType = FrameType.getFrameType(frame);
-        
+        result[0] += new FrameScoreProvider().getScoreProvider(frame);
 
-        int frameScore;
-        if (frame.matches(FrameType.STRIKE_REGEX.getValidRegex())) {
-            frameScore = getScoreForAllBallsKnockedDown();
-        } else if (frame.matches(FrameType.SPARE_REGEX.getValidRegex())) {
-            frameScore = getScoreForAllBallsKnockedDown();
-        } else if (frame.matches(FrameType.TWO_DIGIT_REGEX.getValidRegex())) {
-            frameScore = scoreForTwoDigitNumber(frame);
-        } else if (frame.matches(FrameType.TWO_MISS_REGEX.getValidRegex())) {
-            frameScore = scoreForTwoMissesInAFrame();
-        } else if (frame.matches(FrameType.DIGIT_AND_MISS_REGEX.getValidRegex())) {
-           frameScore =  scoreForADigitAndAMiss(frame);
-        } else if (frame.matches(FrameType.MISS_AND_DIGIT_REGEX.getValidRegex())) {
-            frameScore = scoreForAMissAndADigit(frame);
-        } else {
-            throw new IllegalArgumentException(INVALID_FRAME_EXPRESSION + frame);
-        }
-
-        result[0] +=frameScore;
-
-    }
-
-    private int getScoreForAllBallsKnockedDown() {
-        return 10;
-    }
-
-    private int scoreForTwoDigitNumber(String frame) {
-        int firstDigit = Character.getNumericValue(frame.charAt(0));
-        int secondDigit = Character.getNumericValue(frame.charAt(1));
-        assert firstDigit + secondDigit <= 10 : INVALID_DIGITS_IN_FRAME + frame;
-        return firstDigit + secondDigit;
-    }
-
-    private int scoreForTwoMissesInAFrame() {
-        return 0;
-    }
-
-    private int scoreForADigitAndAMiss(String frame) {
-        int digit = Character.getNumericValue(frame.charAt(0));
-       return digit;
-    }
-
-    private int scoreForAMissAndADigit(String frame) {
-        int digit = Character.getNumericValue(frame.charAt(1));
-       return digit;
     }
 
 }
--- src/main/java/FrameType.java
+++ src/main/java/FrameType.java
@@ -2,14 +2,12 @@ import java.util.Arrays;
 
 public enum FrameType {
     STRIKE_REGEX("^X{1}$"),
-     SPARE_REGEX("^[0-9]{1}\\/{1}$"),
-     TWO_DIGIT_REGEX("^[0-9]{1}[0-9]{1}$"),
-     TWO_MISS_REGEX("^[-]{1}[-]{1}$"),
-     DIGIT_AND_MISS_REGEX ("^[0-9]{1}[-]{1}$"),
-     MISS_AND_DIGIT_REGEX("^[-]{1}[0-9]{1}$");
-    public static final String INVALID_FRAME_EXPRESSION = "invalid frame expression frame:";
-
-
+    SPARE_REGEX("^[0-9]{1}\\/{1}$"),
+    TWO_DIGIT_REGEX("^[0-9]{1}[0-9]{1}$"),
+    TWO_MISS_REGEX("^[-]{1}[-]{1}$"),
+    DIGIT_AND_MISS_REGEX("^[0-9]{1}[-]{1}$"),
+    MISS_AND_DIGIT_REGEX("^[-]{1}[0-9]{1}$");
+    private static final String INVALID_FRAME_EXPRESSION = "invalid frame expression frame:";
     private String validRegex;
 
     FrameType(String regx) {
@@ -19,9 +17,10 @@ public enum FrameType {
     public String getValidRegex() {
         return validRegex;
     }
-    public static FrameType getFrameType(String frame){
+
+    public static FrameType getFrameType(String frame) {
         return Arrays.stream(values()).filter(frameType -> frame.matches(frameType.getValidRegex()))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(INVALID_FRAME_EXPRESSION));
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_FRAME_EXPRESSION + frame));
     }
 }
